a4f6aba3bca8213189206ef6ef1bbf99db8e5268
Andrey Savka <andrewsvk0@gmail.com>
2023-01-05 12:38:53 +0300

    read file

---
+++ lib/domain/models/translation.model.dart
@@ -0,0 +1,76 @@
+class Translation {
+  final String id;
+  final String text;
+  final List<String> translate;
+  final String? description;
+  final String dateAdded;
+  final String dateOfLastTranslate;
+  final int translateRequestsCount;
+  final String category;
+  final int shownTimes;
+  final int correctAnswers;
+
+  Translation({
+    required this.id,
+    required this.text,
+    required this.translate,
+    this.description,
+    required this.dateAdded,
+    required this.dateOfLastTranslate,
+    required this.translateRequestsCount,
+    required this.category,
+    required this.shownTimes,
+    required this.correctAnswers,
+  });
+
+  factory Translation.fromJson(Map<String, dynamic> json) {
+    return Translation(
+      id: json['id'] as String,
+      text: json['text'] as String,
+      description: json['description'] as String?,
+      translate:
+          (json['translate'] as List).map((item) => item as String).toList(),
+      dateAdded: json['dateAdded'] as String,
+      dateOfLastTranslate: json['dateOfLastTranslate'] as String,
+      translateRequestsCount: json['translateRequestsCount'] as int,
+      category: json['category'] as String,
+      shownTimes: json['shownTimes'] as int,
+      correctAnswers: json['correctAnswers'] as int,
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      'id': id,
+      'text': text,
+      'description': description,
+      'translate': translate,
+      'dateAdded': dateAdded,
+      'dateOfLastTranslate': dateOfLastTranslate,
+      'translateRequestsCount': translateRequestsCount,
+      'category': category,
+      'shownTimes': shownTimes,
+      'correctAnswers': correctAnswers,
+    };
+  }
+}
+
+class Translations {
+  final List<Translation> data;
+  Translations({
+    required this.data,
+  });
+
+  factory Translations.fromJson(Map<String, dynamic> json) {
+    return Translations(
+        data: (json['data'] as List<dynamic>)
+            .map((e) => Translation.fromJson(e as Map<String, dynamic>))
+            .toList());
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      'data': data.map((e) => e.toJson()).toList(),
+    };
+  }
+}
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,4 +1,9 @@
+import 'dart:convert';
+import 'dart:io';
+
+import 'package:file_picker/file_picker.dart';
 import 'package:flutter/material.dart';
+import 'package:translate_1/domain/models/translation.model.dart';
 import 'package:translate_1/layouts/default_layout.dart';
 
 class HomePage extends StatelessWidget {
@@ -6,9 +11,34 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const DefaultLayout(
+    return DefaultLayout(
       title: 'Home Page',
-      child: Text('This is main page'),
+      child: Center(
+        child: MaterialButton(
+          onPressed: () {
+            _pickFile();
+          },
+          color: Colors.green,
+          child: const Text(
+            'Select file',
+            style: TextStyle(color: Colors.white),
+          ),
+        ),
+      ),
     );
+  }
+
+  void _pickFile() async {
+    final result = await FilePicker.platform.pickFiles(allowMultiple: false);
+    if (result == null || result.count < 1) {
+      return;
+    }
+    final file = File(result.paths.first!);
+    final contents = await file.readAsString();
+    final json = jsonDecode(contents);
+    final translations = Translations.fromJson(
+      {'data': json},
+    );
+    print('Count: ${translations.data.length}');
   }
 }
